542c7278bde7f15dfb4bb87799286bbf47cd3a1d
Vitor Henrique <vhsouzaeng@gmail.com>
2020-07-17 18:23:10 -0300

    styling pokemon list item

--- src/pages/Main/components/PokemonListItem/PokemonListItem.js
+++ src/pages/Main/components/PokemonListItem/PokemonListItem.js
@@ -2,17 +2,19 @@ import React from 'react';
 import capitalizeString from "../../../../utils/stringUtils";
 import './PokemonListItem.css';
 
-function PokemonListItem({ name, id, spriteUrl, types, pokemonColor }) {
+function PokemonListItem({ name, id, spriteUrl, types }) {
 
     return (
         <li className="pokemonListItem">
-            <img src={spriteUrl} alt={`${name} sprite`} />
-            <span>{capitalizeString(name)}</span>
-            <ul>
-                {types.map(type => (
-                    <li key={`${id}${type.type.name}`}>{type.type.name}</li>
-                ))}
-            </ul>
+            <img className="spriteImage" src={spriteUrl} alt={`${name} sprite`} />
+            <div className="pokemonInfo">
+                <span className="pokemonName">{capitalizeString(name)}</span>
+                <div className="typesList">
+                    {types.map(type => (
+                        <li className={`${type.type.name}`} key={`${id}${type.type.name}`}>{type.type.name}</li>
+                    ))}
+                </div>
+            </div>
         </li>
     );
 }
